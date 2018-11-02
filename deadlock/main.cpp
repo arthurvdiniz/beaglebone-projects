15a5a6e3351eb37675885519edc0c0de962feb13
Arthur <arthurvdinizs@gmail.com>
2018-11-02 18:17:09 -0300

    created main function

--- deadlock/main.cpp
+++ deadlock/main.cpp
@@ -5,8 +5,6 @@
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
-#include <math.h>
 #include <pthread.h>
 #include <sched.h>
 #include "BlackGPIO/BlackGPIO.h"
@@ -14,150 +12,167 @@
 
 using namespace BlackLib;
 
-void *readEntries(void *arg);
-void *setPriorityEntry1(void *arg);
-void *setPriorityEntry2(void *arg);
+void handleCreationThread (int res) {
+    if (res) {
+        printf("Error on creating thread - %d.\n", res);
+        exit(-1);
+    }
+}
 
-float valueEntry1;
-float valueEntry2;
+void handleJoinThread (int res) {
+    if (res) {
+        printf("Error on joining thread - %d.\n", res);
+        exit(-1);
+    }
+}
+
+void *readEntries(void *arg);
+void *setPriorities(void *arg);
+
+void *handleTrain1(void *arg);
+void *handleTrain2(void *arg);
+void *handleTrain3(void *arg);
+void *handleTrain4(void *arg);
+
+float valueEntryTrain1;
+float valueEntryTrain2;
+float valueEntryTrain3;
+float valueEntryTrain4;
 
 int main(int argc, char * argv[]) {
-    int res1, res2, res3;
-    void *resf1, *resf2, *resf3;
+    int res;
+    void *resStatus;
 
-    pthread_t thread1, thread2, thread3;
+    pthread_t thrReadEntries, thrSetPriorities, thrTrain1, thrTrain2, thrTrain3, thrTrain4;
 
-    res1 = pthread_create(&thread1, NULL, readEntries, (void *) 1);
-    res2 = pthread_create(&thread2, NULL, setPriorityEntry1, (void *) 2);
-    res3 = pthread_create(&thread3, NULL, setPriorityEntry2, (void *) 3);
 
-    if (res1 || res2 || res3) {
-        std::cout << "Erro ao criar a thread" << std::endl;
-        exit(-1);
-    }
+    handleCreationThread(pthread_create(&thrReadEntries, NULL, readEntries, (void *) 1));
+    handleCreationThread(pthread_create(&thrSetPriorities, NULL, setPriorities, (void *) 2));
+    handleCreationThread(pthread_create(&thrTrain1, NULL, handleTrain1, (void *) 3));
+    handleCreationThread(pthread_create(&thrTrain2, NULL, handleTrain2, (void *) 4));
+    handleCreationThread(pthread_create(&thrTrain3, NULL, handleTrain3, (void *) 5));
+    handleCreationThread(pthread_create(&thrTrain4, NULL, handleTrain4, (void *) 6));
 
-    res1 = pthread_join(thread1, &resf1);
-    res2 = pthread_join(thread2, &resf2);
-    res3 = pthread_join(thread3, &resf3);
-
-    if (res1 || res2 || res3) {
-        std::cout << "Erro ao fazer o join" << std::endl;
-        exit(-1);
-    }
+    handleJoinThread(pthread_join(thrReadEntries, &resStatus));
+    handleJoinThread(pthread_join(thrSetPriorities, &resStatus));
+    handleJoinThread(pthread_join(thrTrain1, &resStatus));
+    handleJoinThread(pthread_join(thrTrain2, &resStatus));
+    handleJoinThread(pthread_join(thrTrain3, &resStatus));
+    handleJoinThread(pthread_join(thrTrain4, &resStatus));
 
     pthread_exit(NULL);
 
     return 0;
 }
 
-void carga(int k){
-	float f = 0.999999;
-	for (int i=0; i<k; i++) {
-		f = f*f*f*f*f;
-		f = 1.56;
-		for(int j=0; j<k; j++){
-			f = sin(f)*sin(f)*f*f*f;
-		}
-	}
-}
+// void carga(int k){
+// 	float f = 0.999999;
+// 	for (int i=0; i<k; i++) {
+// 		f = f*f*f*f*f;
+// 		f = 1.56;
+// 		for(int j=0; j<k; j++){
+// 			f = sin(f)*sin(f)*f*f*f;
+// 		}
+// 	}
+// }
 
-void *readEntries(void *arg) {
-    ADC en1(AIN0);
-    ADC en2(AIN1);
+// void *readEntries(void *arg) {
+//     ADC en1(AIN0);
+//     ADC en2(AIN1);
 
-    int ret;
+//     int ret;
 
-    pthread_t this_thread = pthread_self();
+//     pthread_t this_thread = pthread_self();
 
-    struct sched_param params;
-    params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+//     struct sched_param params;
+//     params.sched_priority = sched_get_priority_max(SCHED_FIFO);
 
-    ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
+//     ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
 
-    if (ret != 0) {
-        std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
-        exit(-1);    
-    }
+//     if (ret != 0) {
+//         std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
+//         exit(-1);
+//     }
 
-    while (true) {
-        valueEntry1 = en1.getFloatValue();
-        valueEntry2 = en2.getFloatValue();
+//     while (true) {
+//         valueEntry1 = en1.getFloatValue();
+//         valueEntry2 = en2.getFloatValue();
 
-        sleep(0.5);
-    }
+//         sleep(0.5);
+//     }
 
-}
+// }
 
-void *setPriorityEntry1(void *arg) {
-    BlackGPIO led1(GPIO_44, output);
+// void *setPriorityEntry1(void *arg) {
+//     BlackGPIO led1(GPIO_44, output);
 
-    int ret;
-    bool on = false;
+//     int ret;
+//     bool on = false;
 
-    pthread_t this_thread = pthread_self();
-    
-    struct sched_param params;
+//     pthread_t this_thread = pthread_self();
 
-    while (true) {
-        if (valueEntry1 > valueEntry2) {
-            params.sched_priority = sched_get_priority_max(SCHED_FIFO) - sched_get_priority_min(SCHED_FIFO);
-        } else {
-            params.sched_priority = sched_get_priority_min(SCHED_FIFO);
-        }
+//     struct sched_param params;
 
-        ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
+//     while (true) {
+//         if (valueEntry1 > valueEntry2) {
+//             params.sched_priority = sched_get_priority_max(SCHED_FIFO) - sched_get_priority_min(SCHED_FIFO);
+//         } else {
+//             params.sched_priority = sched_get_priority_min(SCHED_FIFO);
+//         }
 
-        if (ret != 0) {
-            std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
-            exit(-1);   
-        }
-        
-        if (on) {
-            led1.setValue(high);
-            carga(1000);
-            on = false;
-        } else {
-            led1.setValue(low);
-            carga(1000);
-            on = true;
-        }
-    }
+//         ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
 
-}
-void *setPriorityEntry2(void *arg) {
+//         if (ret != 0) {
+//             std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
+//             exit(-1);
+//         }
 
-    BlackGPIO led2(GPIO_26, output);
+//         if (on) {
+//             led1.setValue(high);
+//             carga(1000);
+//             on = false;
+//         } else {
+//             led1.setValue(low);
+//             carga(1000);
+//             on = true;
+//         }
+//     }
 
-    int ret;
-    bool on = false;
+// }
+// void *setPriorityEntry2(void *arg) {
 
-    pthread_t this_thread = pthread_self();
-    
-    struct sched_param params;
+//     BlackGPIO led2(GPIO_26, output);
 
-    while (true) {
-        if (valueEntry2 > valueEntry1) {
-            params.sched_priority = sched_get_priority_max(SCHED_FIFO) - sched_get_priority_min(SCHED_FIFO);
-        } else {
-            params.sched_priority = sched_get_priority_min(SCHED_FIFO);
-        }
+//     int ret;
+//     bool on = false;
 
-        ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
+//     pthread_t this_thread = pthread_self();
 
-        if (ret != 0) {
-            std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
-            exit(-1);     
-        }
-        if (on) {
-            led2.setValue(high);
-            carga(1000);
-            on = false;
-        } else {
-            led2.setValue(low);
-            carga(1000);
-            on = true;
-        }
-    }
-}
+//     struct sched_param params;
+
+//     while (true) {
+//         if (valueEntry2 > valueEntry1) {
+//             params.sched_priority = sched_get_priority_max(SCHED_FIFO) - sched_get_priority_min(SCHED_FIFO);
+//         } else {
+//             params.sched_priority = sched_get_priority_min(SCHED_FIFO);
+//         }
+
+//         ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
+
+//         if (ret != 0) {
+//             std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
+//             exit(-1);
+//         }
+//         if (on) {
+//             led2.setValue(high);
+//             carga(1000);
+//             on = false;
+//         } else {
+//             led2.setValue(low);
+//             carga(1000);
+//             on = true;
+//         }
+//     }
+// }
 
 // http://www.yonch.com/tech/82-linux-thread-priority
