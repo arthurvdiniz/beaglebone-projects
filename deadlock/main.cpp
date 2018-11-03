acb5de8b64e8889dfccb335cfea523ff06bd519f
Arthur <arthurvdinizs@gmail.com>
2018-11-03 10:28:45 -0300

    handlePriority function

--- deadlock/main.cpp
+++ deadlock/main.cpp
@@ -10,7 +10,7 @@
 #include "BlackGPIO/BlackGPIO.h"
 #include "ADC/Adc.h"
 
-#define NUM_TRAINS 3
+#define NUM_TRAINS 4
 #define MAX_VALUE 1.8
 
 using namespace BlackLib;
@@ -23,6 +23,8 @@ void *handleTrain1(void *arg);
 void *handleTrain2(void *arg);
 void *handleTrain3(void *arg);
 void *handleTrain4(void *arg);
+
+pthread_t thr[NUM_TRAINS];
 
 float valueEntries[NUM_TRAINS];
 
@@ -40,6 +42,13 @@ void handleJoinThread(int res) {
     }
 }
 
+void handlePriority(int res) {
+    if (res) {
+        printf("Error in setting thread prio - %d.\n", res);
+        exit(-1);
+    }
+}
+
 int main(int argc, char * argv[]) {
     void *resStatus;
 
@@ -49,24 +58,23 @@ int main(int argc, char * argv[]) {
     handleCreationThread(pthread_create(&thrReadEntries, NULL, readEntries, (void *) 1));
     handleCreationThread(pthread_create(&thrSetPriorities, NULL, setPriorities, (void *) 2));
 
+    handleCreationThread(pthread_create(&thr[0], NULL, handleTrain1, (void *) 3));
+    handleCreationThread(pthread_create(&thr[1], NULL, handleTrain2, (void *) 4));
+    handleCreationThread(pthread_create(&thr[2], NULL, handleTrain3, (void *) 5));
+    handleCreationThread(pthread_create(&thr[3], NULL, handleTrain4, (void *) 6));
+
     handleJoinThread(pthread_join(thrReadEntries, &resStatus));
     handleJoinThread(pthread_join(thrSetPriorities, &resStatus));
+
+    handleJoinThread(pthread_join(thr[0], &resStatus));
+    handleJoinThread(pthread_join(thr[1], &resStatus));
+    handleJoinThread(pthread_join(thr[2], &resStatus));
+    handleJoinThread(pthread_join(thr[3], &resStatus));
 
     pthread_exit(NULL);
 
     return 0;
 }
-
-// void carga(int k){
-// 	float f = 0.999999;
-// 	for (int i=0; i<k; i++) {
-// 		f = f*f*f*f*f;
-// 		f = 1.56;
-// 		for(int j=0; j<k; j++){
-// 			f = sin(f)*sin(f)*f*f*f;
-// 		}
-// 	}
-// }
 
 void *readEntries(void *arg) {
     ADC train1(AIN0);
@@ -74,19 +82,12 @@ void *readEntries(void *arg) {
     ADC train3(AIN2);
     ADC train4(AIN3);
 
-    int ret;
-
     pthread_t this_thread = pthread_self();
 
     struct sched_param params;
     params.sched_priority = sched_get_priority_max(SCHED_RR);
 
-    ret = pthread_setschedparam(this_thread, SCHED_RR, &params);
-
-    if (ret != 0) {
-        std::cout << "Unsuccessful in setting thread realtime prio" << std::endl;
-        exit(-1);
-    }
+    handlePriority(pthread_setschedparam(this_thread, SCHED_RR, &params));
 
     while (true) {
         valueEntries[0] = train1.getFloatValue();
@@ -102,31 +103,19 @@ void *setPriorities(void *arg) {
     int ret;
     struct sched_param params;
 
-    void *resStatus;
+    pthread_t this_thread = pthread_self();
+    params.sched_priority = sched_get_priority_max(SCHED_RR) - 1;
 
-    pthread_t thr[NUM_TRAINS];
-
-    handleCreationThread(pthread_create(&thr[0], NULL, handleTrain1, (void *) 3));
-    handleCreationThread(pthread_create(&thr[1], NULL, handleTrain2, (void *) 4));
-    handleCreationThread(pthread_create(&thr[2], NULL, handleTrain3, (void *) 5));
-    handleCreationThread(pthread_create(&thr[3], NULL, handleTrain4, (void *) 6));
+    handlePriority(pthread_setschedparam(this_thread, SCHED_RR, &params));
 
     while (true) {
-
         for (int i = 0; i < NUM_TRAINS; i++) {
-            params.sched_priority = (valueEntries[i] / MAX_VALUE) * (sched_get_priority_max(SCHED_RR) - 1);
-
-            ret = pthread_setschedparam(thr[i], SCHED_RR, &params);
-
-            if (ret) {
-                printf("Error in setting thread prio - %d.\n", ret);
-                exit(-1);
-            }
+            params.sched_priority = (int) ((valueEntries[i] / MAX_VALUE) * (sched_get_priority_max(SCHED_RR) - 2));
+            handlePriority(pthread_setschedparam(thr[i], SCHED_RR, &params));
         }
 
         sleep(0.5);
     }
-
 }
 
 void *handleTrain1() {
